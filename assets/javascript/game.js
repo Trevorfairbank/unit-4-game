07a54adf0a8d40f522bad2d937649b963956b768
Trevor Fairbank <Trevorfairbank92@gmail.com>
2019-05-28 22:08:49 -0700

    updated and fixed javascript to work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,65 @@
-//declare variables
-
 //You will be given a random number at the start of the game.
-var High = 100
-var Low = 1
-var randomNum = Math.floor(Math.random() * (1 + High - Low)) + Low
+//declare function that creates a random number
+function getRandomInt(min,max){
+    return Math.floor(Math.random() * max) + min;
+}
 
-console.log(randomNum);
 
-var showNumber = document.getElementById("randomNumber");
-//display randomNumber
-showNumber.textContent = randomNum;
+var wins = 0;
+var losses = 0;
+var score = 0;
+var randomNum = getRandomInt(19,120);
+
+//display number
+$("#randomNumber").text(randomNum);
+
+//display wins
+$("#wins").text(" " + wins)
+//display losses
+$("#losses").text(" " + losses)
 
 //There are four crystals below. By clicking on a crystal you will add a specific amount of points to your total score.
+//declaring random number for each crystal button
+for(var i = 0; i < $(".crystal").length;i++){
+    var crystalVal = getRandomInt(1,12);
+    $("#crystal-"+i).val(crystalVal);
+}
+
+//reset game function
+function resetGame() {
+    randomNum = getRandomInt(19,120);
+    $("#randomNumber").text(randomNum);
+    score = 0;
+    $("#totalScore").text(score);
+
+    for(var i = 0; i < $(".crystal").length;i++){
+        $("#crystal-"+i).val(crystalVal);
+    }    
+}
+
+
+//clicking on gems
+$(".crystal").on("click", function(){
+    $("#totalScore").text("");
+    //get value from gem
+    var crystalTotal = parseInt($(this).val());
+    score += crystalTotal;
+    $("#totalScore").text(score);
+    console.log(crystalTotal);
+
+    if(score === randomNum){
+        // alert("You win!");
+        wins++;
+        $("#wins").text(" " + wins)
+        resetGame();
+    }
+    else if(score > randomNum){
+        // alert("You lose!");
+        losses++;
+        $("#losses").text(" " + losses)
+        resetGame();
+    }
+});
 
 //You win the game by matching your total score to the random number, you lose the game if your total score goes above the random number.
 
